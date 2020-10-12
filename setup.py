e80909acc0e7ce2e9f6400f76dace6294cfb8b1b
Matteo Sandrin <matteo.sandrin@hotmail.it>
2020-10-12 11:27:57 -0400

    Add Python library boilerplate code

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+	name='structoscope',
+	packages=find_packages(include=['structoscope']),
+	version='0.1.0',
+	description='A Python library for visualizing and inspecting data structures',
+	author='Matteo Sandrin',
+	license='MIT',
+	install_requires=[],
+	setup_requires=['pytest-runner'],
+	tests_require=['pytest'],
+	test_suite='tests',
+)
